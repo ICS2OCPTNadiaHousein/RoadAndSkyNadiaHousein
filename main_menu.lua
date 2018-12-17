dac7b081ee1ac24b2b1d295fd66bee5060afff0c
nadiacoleman <nadia.coleman@ocsbstudent.ca>
2018-12-17 13:58:16 -0500

    Update main_menu.lua

--- main_menu.lua
+++ main_menu.lua
@@ -235,6 +235,7 @@ function scene:show( event )
     -- Called when the scene is now on screen.
     -- Insert code here to make the scene come alive.
     -- Example: start timers, begin animation, play audio, etc.
+    audio.play(baqckgroundSoundChannel)
     elseif ( phase == "did" ) then       
         
 
@@ -260,7 +261,7 @@ function scene:hide( event )
         -- Called when the scene is on screen (but is about to go off screen).
         -- Insert code here to "pause" the scene.
         -- Example: stop timers, stop animation, stop audio, etc.
-        audio.stop(backgroundSoundChannel)
+        --audio.stop(backgroundSoundChannel)
 
 
     -----------------------------------------------------------------------------------------
